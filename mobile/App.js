d04d5250bb674532209064db5a1101cfdfe95896
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-10-18 07:56:20 +0530

    razorpay implemented in mobile application

--- mobile/App.js
+++ mobile/App.js
@@ -1,112 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
+import RazorpayCheckout from 'react-native-razorpay';
+const App = () => {
+  const payNow = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5337/generate-razorpay-id`,
+        {
+          headers: {'Content-Type': 'application/json'},
+          method: 'POST',
+          body: JSON.stringify({amount: 200}),
+        },
+      );
+      const data = await response.json();
+      const options = {
+        key: 'rzp_test_VULKZYu2VDqjkD',
+        currency: data.currency,
+        amount: data.amount.toString(),
+        order_id: data.id,
+        name: 'Node React Razorpay',
+        description: 'Node React Razorpay',
+        prefill: {
+          name: 'Ashirbad Panigrahi',
+          email: 'ashirbada@ashirbada.com',
+          contact: '+91 9123456789',
+        },
+      };
+      const {razorpay_payment_id} = await RazorpayCheckout.open(options);
+      console.log(razorpay_payment_id);
+    } catch (error) {
+      console.log(error);
+    }
   };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={{flex: 1, alignItems: 'center'}}>
+      <Text style={{fontWeight: 'bold', fontSize: 18, marginVertical: 15}}>
+        Welcome To React Native Razorpay
+      </Text>
+      <Button title="Pay Now" onPress={payNow} />
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+
+const styles = StyleSheet.create({});
